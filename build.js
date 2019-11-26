feff9ed2d75e953d0eac1a2450b3ae9eb7db65d9
Jonas Johansson <jns.johansson@gmail.com>
2019-11-26 23:59:40 +0100

    update

---
+++ build.js
@@ -0,0 +1,37 @@
+'use strict';
+
+const builder = require('electron-builder');
+const Platform = builder.Platform;
+
+// https://github.com/electron-userland/electron-builder#programmatic-usage
+// https://www.electron.build/configuration/dmg
+// Promise is returned
+builder
+	.build({
+		targets: Platform.MAC.createTarget(),
+		// https://github.com/electron-userland/electron-builder/issues/1313
+		// targets: Platform.WINDOWS.createTarget('portable'),
+		config: {
+			appId: 'se.jonasjohansson.homie',
+			mac: {
+				category: 'public.app-category.social-networking',
+				target: ['dir', 'dmg'],
+				electronUpdaterCompatibility: '>=2.16.0'
+			},
+			dmg: {
+				title: '${name}',
+				backgroundColor: '#999',
+				// icon: 'build/icon-negative.png',
+				iconSize: 100
+			}
+		}
+	})
+	.then(() => {
+		// handle result
+	})
+	.catch(error => {
+		// handle error
+	});
+
+// build options, see https://goo.gl/QQXmcV
+// https://www.electron.build/configuration/mac
